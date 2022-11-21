17d545e9ba04ba0782e23f8fa776cb1a512aa81b
Frank27 <39018610+goooochi@users.noreply.github.com>
2022-11-21 18:11:40 +0900

    Initial Commit

---
+++ RitsLunch/RitsLunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RitsLunchApp.swift
+//  RitsLunch
+//
+//  Created by Yuuki Yamaguchi on 2022/11/21.
+//
+
+import SwiftUI
+
+@main
+struct RitsLunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
