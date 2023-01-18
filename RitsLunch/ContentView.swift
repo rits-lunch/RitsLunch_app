be65b849057306fc22e060007cf65cc5675ccb29
澤木柊斗 <sawakishuto@sawakihiiragitonoMacBook-Pro.local>
2023-01-18 23:13:20 +0900

    テスト

--- RitsLunch/ContentView.swift
+++ RitsLunch/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
             
             
             
+            
+            
                 .frame(width: 414, height: 50)
                 .background(.red)
             NavigationView {
